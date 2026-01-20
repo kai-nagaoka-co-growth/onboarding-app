27924001afd29abf12608f06033faea76ae11329
kai-nagaoka-co-growth <kai.nagaoka@co-growth.jp>
2026-01-20 16:15:21 +0900

    create_memo_create_api

--- backend/app/controllers/api/v1/memos_controller.rb
+++ backend/app/controllers/api/v1/memos_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::MemosController < ApplicationController
     @memos = Memo.all.order(created_at: :desc)
     render json: @memos.as_json, status: :ok
   end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      render json: @memo.as_json, status: :created
+    else
+      render json: { errors: @memo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:title, :body)
+  end
 end
