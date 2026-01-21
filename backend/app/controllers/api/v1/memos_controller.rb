b34b37cfa0095e23437f2eddfad92044f3728c97
kai-nagaoka-co-growth <kai.nagaoka@co-growth.jp>
2026-01-21 10:44:29 +0900

    リファクタリング

--- backend/app/controllers/api/v1/memos_controller.rb
+++ backend/app/controllers/api/v1/memos_controller.rb
@@ -3,21 +3,21 @@ class Api::V1::MemosController < ApplicationController
   before_action :set_memo, only: [:update, :destroy]
 
   def index
-    @memos = Memo.all.order(created_at: :desc).pluck(:title,:updated_at)
-    render json: @memos.as_json, status: :ok
+    memos = Memo.all.order(created_at: :desc).pluck(:title,:updated_at)
+    render json: memos.as_json, status: :ok
   end
 
   def show
-    @memo = Memo.includes(:comments).find(params[:id])
-    render json: { memo: @memo.as_json(include: :comments) }, status: :ok
+    memo = Memo.includes(:comments).find(params[:id])
+    render json: { memo: memo.as_json(include: :comments) }, status: :ok
   end
 
   def create
-    @memo = Memo.new(memo_params)
-    if @memo.save
-      render json: @memo.as_json, status: :created
+    memo = Memo.new(memo_params)
+    if memo.save
+      render json: memo.as_json, status: :created
     else
-      render json: { errors: @memo.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
